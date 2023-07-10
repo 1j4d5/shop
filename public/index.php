476c7342a46bb2d10fc1babdf1faf956a8121231
1j4d5 <ijads.main@gmail.com>
2023-07-11 00:51:26 +0500

    -implemented routing

--- public/index.php
+++ public/index.php
@@ -1,6 +1,44 @@
 <?php
-echo "<pre>";
+$routes = [
+    "/" => function ( $params = null) {
+        return "welcome home";
+    },
+    "/users" => function ($params=null) {
+        return "users index";
+    },
+    "/users/{id}" => function ($params) {
+        return "show user from id".$params["id"]?? "";
+    },  
+    "/users/{id}/edit" => function ($params) {
+        return "show users edit from id".$params?? "";
+    }, 
+    "/users/{id}/delete" => function ($params) {
+        return "show users edit from id ".$params?? "";
+    }    
+];
+$uri = $_SERVER["REQUEST_URI"];
+$callable = $routes[$uri]??null;
+$params = [];
+if (! $callable) {
+    foreach ($routes as $route => $function) {
 
-echo $_SERVER["REQUEST_URI"];
+       if (str_contains($route, "{id}")) {
 
-echo "</pre>";
+        $pattern = str_replace("/","\/",$route);
+
+        $pattern = str_replace("{id}", "(\d+)", $pattern);
+
+        preg_match_all("/".$pattern."-/", $uri."-", $matches);
+
+        if (isset($matches[sizeof($matches)-1][0])) {
+
+            $params["id"] = $matches[sizeof($matches)-1][0];
+
+            $callable = $function;
+
+            break;
+        }        
+       }
+    }
+}
+echo $callable($params);
